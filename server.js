838d7eb2928282439f64888969f38704bb9e6ebb
Maszal_08 <146170469+ZalStack@users.noreply.github.com>
2026-04-21 02:23:38 +0700

    Remove default port fallback in server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const errorHandler = require('./middleware/errorHandler');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 // Middleware
 app.use(cors());
